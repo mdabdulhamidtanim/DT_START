149a09e52c7983f2376908155ce06d9132252276
dhruvtanim <dhruvtanim@gmail.com>
2023-12-18 00:10:47 +0600

    Add project files.

---
+++ START/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace START.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
